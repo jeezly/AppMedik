2303c985b7f45fa9986321ee4b58b3ef9e3f7e1e
Little-Twig <hugo_armando@live.com>
2025-04-05 17:00:39 -0600

    Implementaciones de gradle Permisos de manifest Creacion de ApiClient para usar servicios Inicio de servicios de medico

--- app/src/main/java/org/utl/calculadoradosificadora/service/MedicoService.java
+++ app/src/main/java/org/utl/calculadoradosificadora/service/MedicoService.java
@@ -5,9 +5,22 @@ import org.utl.calculadoradosificadora.model.Medico;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface MedicoService {
     @GET("getAllMedicos")
     Call<List<Medico>> getAllMedicos();
+    @POST("InsertMedico")
+    Call<Medico> insertMedico(@Body Medico medico);
+
+    @PUT("UpdateMedico/{id}")
+    Call<Medico> updateMedico(@Path("id") int id, @Body Medico medico);
+
+    @DELETE("DeleteMedico/{id}/inhabilitar")
+    Call<Void> deleteMedico(@Path("id") int id);
 }
